d9096ae69c54451ec73b896a64fb66959965514a
//PatrykSzereluk <Patryk@Tatoo-Komputer>
2018-05-26 14:40:08 +0200

    main

---
+++ src/skrzynka/Skrzynka.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package skrzynka;
+
+import Widok.Ekran;
+
+/**
+ *
+ * @author Patryk
+ */
+public class Skrzynka {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Ekran e = new Ekran();
+        e.setVisible(true);
+    }
+    
+}
